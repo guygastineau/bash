2d2b7a066f607806c7a293ee19f5644d05a89bdd
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-07-24 20:15:39 -0400

    dded two generator templates and tests

--- exercises/reverse-string/old_test.sh
+++ exercises/reverse-string/old_test.sh
@@ -1,38 +1,41 @@
 #!/usr/bin/env bats
 
-# Version = 1.1.0
-
-@test "an empty string" {
+@test "An empty string" {
   #skip
   run bash reverse_string.sh ""
+
   [ "$status" -eq 0 ]
-  [ "$output" == "" ]
+  [ "$output" = "" ]
 }
 
-@test "a word" {
+@test "A word" {
   skip
   run bash reverse_string.sh "robot"
+
   [ "$status" -eq 0 ]
-  [ "$output" == "tobor" ]
+  [ "$output" = "tobor" ]
 }
 
-@test "a capitalized word" {
+@test "A capitalised word" {
   skip
   run bash reverse_string.sh "Ramen"
+
   [ "$status" -eq 0 ]
-  [ "$output" == "nemaR" ]
+  [ "$output" = "nemaR" ]
 }
 
-@test "a sentence with punctuation" {
+@test "A sentence with punctuation" {
   skip
   run bash reverse_string.sh "I'm hungry!"
+
   [ "$status" -eq 0 ]
-  [ "$output" == "!yrgnuh m'I" ]
+  [ "$output" = "!yrgnuh m'I" ]
 }
 
-@test "a palindrome" {
+@test "A palindrome" {
   skip
   run bash reverse_string.sh "racecar"
+
   [ "$status" -eq 0 ]
-  [ "$output" == "racecar" ]
+  [ "$output" = "racecar" ]
 }
